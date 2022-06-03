83649292830cd548cc3d7373e651158b9d3bae94
rostandmessi2@gmail.com <rostandmessi2@gmail.com>
2022-06-03 16:14:36 +0100

    mirshopbackend

--- index.js
+++ index.js
@@ -2,13 +2,24 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-dotenv.config();
+const userRoute = require("./routes/user")
+const authRoute = require("./routes/auth")
 
+dotenv.config();  
+// cat .gitignore
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("DBConnection successful"))
   .catch((err) => console.log(err));
 
-app.listen(5000, () => {
+app.use(express.json())
+
+app.use("/api/auth",authRoute )
+app.use("/api/users",userRoute )
+
+
+
+
+app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is ok");
 });
